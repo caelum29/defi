5b312728a88e468200ba7a39f37bbcea373d2a4d
Artem Sorochynskyi <a.sorochynskyi@softonix.org>
2023-03-09 10:49:05 +0200

    final

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,18 @@
-import { HttpException, Inject, Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import {
+  HttpException,
+  Inject,
+  Injectable,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 import { createFile } from 'src/helpers/storage.helper';
 import { Web3Service } from 'src/web3/web3.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
   constructor(
     private readonly web3Service: Web3Service,
+    private schedulerRegistry: SchedulerRegistry,
     @Inject('ADDRESSES')
     private readonly addresses: { tokenAddresses: string[]; address: string },
   ) {}
@@ -14,18 +20,6 @@ export class AppService {
     try {
       const address = this.web3Service.checkAddress(this.addresses.address);
       const weis = await this.web3Service.getBalances(address);
-
-      // const tokenAddresses = [
-      //   '0x58b6A8A3302369DAEc383334672404Ee733aB239',
-      //   // '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359',
-      //   '0x61fd1c62551850D0c04C76FcE614cBCeD0094498',
-      //   '0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39',
-      //   '0xBBbbCA6A901c926F240b89EacB641d8Aec7AEafD',
-      //   '0xBDDab785b306BCD9fB056Da189615Cc8eCE1D823',
-      //   '0xe530441f4f73bDB6DC2fA5aF7c3fC5fD551Ec838',
-      //   '0x4c1C4957D22D8F373aeD54d0853b090666F6F9De',
-      //   '0xf230b790E05390FC8295F4d3F60332c93BEd42e2',
-      // ];
 
       const tokenBalances = await Promise.all(
         this.addresses.tokenAddresses.map(async (contractAddress) => {
@@ -46,18 +40,40 @@ export class AppService {
     }
   }
 
-  @Cron(CronExpression.EVERY_MINUTE)
-  async triggerCronJob() {
-    console.log('Calling the method every minute');
+  // it's working but not correctly
+  // @Cron(CronExpression.EVERY_MINUTE, { name: 'writeBalances' })
+  // async triggerCronJob() {
+  //   console.log('Calling the method every minute');
+  //
+  //   await createFile(
+  //     './storage/',
+  //     'balances.json',
+  //     JSON.stringify(
+  //       { ...(await this.getBalances()), time: new Date() },
+  //       null,
+  //       2,
+  //     ),
+  //   );
+  // }
 
-    await createFile(
-      './storage/',
-      'balances.json',
-      JSON.stringify(
-        { ...(await this.getBalances()), time: new Date() },
-        null,
-        2,
-      ),
-    );
+  // this one is correct bun maybe not so fancy
+  onModuleInit() {
+    const callback = async () => {
+      await createFile(
+        './storage/',
+        'balances.json',
+        JSON.stringify(
+          { ...(await this.getBalances()), time: new Date() },
+          null,
+          2,
+        ),
+      );
+      this.schedulerRegistry.deleteTimeout('writeFile');
+
+      this.onModuleInit();
+    };
+
+    const timeout = setTimeout(callback, 1000 * 60);
+    this.schedulerRegistry.addTimeout('writeFile', timeout);
   }
 }
